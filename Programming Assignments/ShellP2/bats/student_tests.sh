13168143e6a120ae84d6003b2691be8b3f7ac95c
Jacob Chen <jc4647@drexel.edu>
2025-02-19 23:29:07 -0500

    Assignment done no questions

--- Programming Assignments/ShellP2/bats/student_tests.sh
+++ Programming Assignments/ShellP2/bats/student_tests.sh
@@ -12,3 +12,80 @@ EOF
     # Assertions
     [ "$status" -eq 0 ]
 }
+
+@test "Echo command" {
+    run ./dsh <<EOF
+echo Hello, World!
+EOF
+
+    expected_output="Hello, World!"
+
+    [ "$status" -eq 0 ]
+    [[ "$output" == *"$expected_output"* ]]
+}
+
+@test "Change directory and Check pwd" {
+    run ./dsh <<EOF 
+cd /tmp
+pwd
+EOF
+
+    expected_output="/tmp"
+
+
+    [ "$status" -eq 0 ]
+    [[ "$output" == *"$expected_output"* ]]
+}
+
+@test "Starts and Exits" {
+    run ./dsh <<EOF
+exit
+EOF
+    [ "$status" -eq 0 ]
+}
+
+@test "Quoted arguments" {
+    run ./dsh <<EOF
+echo "Hello BATS"
+EOF
+
+    expected_output="Hello BATS"
+
+    [ "$status" -eq 0 ]
+    [[ "$output" == *"$expected_output"* ]]
+}
+
+@test "Check environment variables are listed" {
+    run ./dsh <<EOF
+env
+EOF
+
+    expected_output="HOME="
+
+    [ "$status" -eq 0 ]
+    [[ "$output" == *"$expected_output"* ]]
+}
+
+@test "Error rc command" {
+    run ./dsh <<EOF
+cd /invalid/path
+rc
+EOF
+    [ "$status" -eq 0 ]
+    [[ "$output" == *"2"* ]] 
+}
+
+@test "Success rc command" {
+    run ./dsh <<EOF
+ls
+rc
+EOF
+    [ "$status" -eq 0 ]
+    [[ "$output" == *"0"* ]]
+}
+
+@test "Test cd with too many arguements" {
+    run ./dsh <<< "cd dir1 dir2"
+    [ "$status" -eq 0 ]
+    [[ "$output" == *"cd: Too many arguments"* ]]
+}
