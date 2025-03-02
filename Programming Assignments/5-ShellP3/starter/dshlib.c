8ea51dcdb97d582e57dca2d3c54ec8a6af8b3469
Jacob Chen <jc4647@drexel.edu>
2025-03-02 14:54:25 -0500

    Copied code from A4 to A5

--- Programming Assignments/5-ShellP3/starter/dshlib.c
+++ Programming Assignments/5-ShellP3/starter/dshlib.c
@@ -52,8 +52,237 @@
  *  Standard Library Functions You Might Want To Consider Using (assignment 2+)
  *      fork(), execvp(), exit(), chdir()
  */
+
+
+
+int last_rc = 0;
+
+int alloc_cmd_buff(cmd_buff_t *cmd_buff) {
+    cmd_buff->_cmd_buffer = malloc(SH_CMD_MAX);
+    if (!cmd_buff->_cmd_buffer) {
+        return ERR_MEMORY;
+    }
+
+    memset(cmd_buff->_cmd_buffer, 0, SH_CMD_MAX);
+    cmd_buff->argc = 0;
+    for (int i = 0; i < CMD_ARGV_MAX; i++) {
+        cmd_buff->argv[i] = NULL;
+    }
+
+    return OK;
+ }
+
+
+int free_cmd_buff(cmd_buff_t *cmd_buff) {
+    if (cmd_buff->_cmd_buffer) {
+        free(cmd_buff->_cmd_buffer);
+        cmd_buff->_cmd_buffer = NULL;
+    }
+
+    return OK;
+ }
+
+
+int clear_cmd_buff(cmd_buff_t *cmd_buff) {
+    memset(cmd_buff->_cmd_buffer, 0, SH_CMD_MAX);
+    cmd_buff->argc = 0;
+    for (int i = 0; i < CMD_ARGV_MAX; i++) {
+        cmd_buff->argv[i] = NULL;
+    }
+
+    return OK;
+ }
+
+
+int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff) {
+    clear_cmd_buff(cmd_buff);
+
+    char *input = cmd_buff->_cmd_buffer;
+    strncpy(input, cmd_line, SH_CMD_MAX - 1);
+    input[SH_CMD_MAX - 1] = '\0';
+
+    int argc = 0;
+    char *cur = input;
+    char *start = input;
+    int inQuotes = 0;
+    char quote = 0;
+
+    while (*cur) {
+        if (inQuotes) {
+            if (*cur == quote) {
+                inQuotes = 0;
+                *cur = '\0';
+                cmd_buff->argv[argc++] = start;
+                start = cur + 1;
+            }
+            cur++;
+        } else {
+            if (*cur == '"' || *cur == '\'') {
+                inQuotes = 1;
+                quote = *cur;
+                *cur = '\0';
+                if (cur > start) {
+                    cmd_buff->argv[argc++] = start;
+                }
+                start = cur + 1;
+                cur++;
+            } else if (isspace(*cur)) {
+                *cur = '\0';
+                if (cur > start) {
+                    cmd_buff->argv[argc++] = start;
+                }
+                start = cur + 1;
+                cur++;
+            } else {
+                cur++;
+            }
+        }
+    }
+
+    if (cur > start) {
+        cmd_buff->argv[argc++] = start;
+    }
+    cmd_buff->argc = argc;
+    if (argc == 0) {
+        return WARN_NO_CMDS;
+    } else {
+        return OK;
+    }
+}
+
+Built_In_Cmds match_command(const char *input) {
+    if (strcmp(input, EXIT_CMD) == 0) {
+        return BI_CMD_EXIT;
+    } else if (strcmp(input, "cd") == 0) {
+        return BI_CMD_CD;
+    } else if (strcmp(input, "rc") == 0) {
+        return BI_RC;
+    } else {
+        return BI_NOT_BI;
+    }
+}
+
+Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd) {
+    if (cmd->argc == 0) {
+        return BI_NOT_BI;
+    }
+
+    if (strcmp(cmd->argv[0], "cd") == 0) {
+        if (cmd->argc == 1) {
+            last_rc = 0;
+            return BI_CMD_CD;
+        } else if (cmd->argc == 2) {
+            if (chdir(cmd->argv[1]) != 0) {
+                last_rc = errno;
+                perror("cd");
+            } else {
+                last_rc = 0;
+            }
+
+            return BI_CMD_CD;
+        } else {
+            last_rc = ERR_CMD_OR_ARGS_TOO_BIG;
+            fprintf(stderr, "cd: Too many arguments\n");
+            return BI_CMD_CD;
+        }
+    } else if (strcmp(cmd->argv[0], "exit") == 0) {
+        return BI_CMD_EXIT;
+    } else if (strcmp(cmd->argv[0], "rc") == 0) {
+        printf("%d\n", last_rc);
+        return BI_RC;
+    }
+
+    return BI_NOT_BI;
+}
+
+int execCmd(cmd_buff_t *cmd) {
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        return ERR_EXEC_CMD;
+    } else if (pid == 0) {
+        execvp(cmd->argv[0], cmd->argv);
+        switch (errno) {
+            case ENOENT:
+                fprintf(stderr, "Command not found\n");
+                break;
+            case EACCES:
+                fprintf(stderr, "Permission denied\n");
+                break;
+            default:
+                fprintf(stderr, "Command failed");
+        }
+        exit(errno);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status)) {
+            last_rc = WEXITSTATUS(status);
+        } else {
+            last_rc = -1;
+        }
+    }
+    return OK;
+}
+
 int exec_local_cmd_loop()
 {
-   
+    char *cmd_buff = malloc(SH_CMD_MAX * sizeof(char));
+    
+    if (!cmd_buff) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return ERR_MEMORY;
+    }
+
+    int rc = 0;
+    cmd_buff_t cmd;
+
+    if (alloc_cmd_buff(&cmd) != OK) {
+        fprintf(stderr, "Failed to allocate command buffer\n");
+        free(cmd_buff);
+        return ERR_MEMORY;
+    }
+    
+    // TODO IMPLEMENT MAIN LOOP
+    while (1) {
+        printf("%s", SH_PROMPT);
+        if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL) {
+            printf("\n");
+           break;
+        }
+
+        cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
+
+
+        if (strcmp(cmd_buff, EXIT_CMD) == 0) {
+            break;
+        }
+
+        if (strlen(cmd_buff) == 0) {
+            printf(CMD_WARN_NO_CMD);
+            continue;
+        }
+
+        rc = build_cmd_buff(cmd_buff, &cmd);
+        if (rc == WARN_NO_CMDS) {
+            printf(CMD_WARN_NO_CMD);
+            continue;
+        } else if (rc == ERR_TOO_MANY_COMMANDS) {
+            printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+            continue;
+        }
+        // TODO IMPLEMENT if built-in command, execute builtin logic for exit, cd (extra credit: dragon)
+        // the cd command should chdir to the provided directory; if no directory is provided, do nothing
+        Built_In_Cmds result = exec_built_in_cmd(&cmd);
+        if (result == BI_CMD_EXIT) {
+            break;
+        } else if (result == BI_NOT_BI) {
+            if (cmd.argc > 0) {
+                execCmd(&cmd);
+            }
+        }
+    }
+    free_cmd_buff(&cmd);
+    free(cmd_buff);
     return OK;
 }
